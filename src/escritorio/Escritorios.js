7e003f451e173b537ddfb101595cde7e656b31f2
jhonhtr <129766078+jhonhtr@users.noreply.github.com>
2024-10-18 11:38:25 -0500

    Update Escritorios.js

--- src/escritorio/Escritorios.js
+++ src/escritorio/Escritorios.js
@@ -134,7 +134,7 @@ const Escritorios = () => {
                 <div className='pago1'>
                     <div className='pago2'>
                         <h4>Para realizar tu pedido <br/><br/>
-                        Contactar al: <a target="_blank" rel="noopener noreferrer" 
+                        Contactar al: <a className='contacto' target="_blank" rel="noopener noreferrer" 
                         href="https://wa.me/+573205119854">+56 949165634</a><br/> <br/><br/>
                         Fabricante: Michael Amín Tierradentro R.<br/></h4>                        
                         <div className='botones'>                            
