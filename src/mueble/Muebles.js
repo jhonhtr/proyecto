5f673c43bd9cbb47bdd4b982ec063b2c7a26d813
jhonhtr <129766078+jhonhtr@users.noreply.github.com>
2024-10-18 14:05:22 -0500

    Update Muebles.js

--- src/mueble/Muebles.js
+++ src/mueble/Muebles.js
@@ -430,15 +430,15 @@ const Muebles = () => {
 
             <footer>
                 <section className='pie'>
-                    <p>Desarrollador: Harris Tierradentro</p>               
-                    <a target="_blank" rel="noopener noreferrer" 
+                    <p>CEO: Michael Amín Tierradentro</p>               
+                    Contacto: <a target="_blank" rel="noopener noreferrer" 
+                    href="https://wa.me/+573205119854">+56949165634</a><br/>                        
+                </section>
+                <p>Desarrollador: Harris Tierradentro<br/>
+                <a target="_blank" rel="noopener noreferrer" 
                     href="https://wa.me/+573205119854">+573205119854</a>
                      / <a target="_blank" rel="noopener noreferrer" 
-                     href="mailto:jhonhtr@gmail.com">jhonhtr@gmail.com </a>                    
-                </section>
-                <p>CEO: Michael Amín Tierradentro<br/>
-                Contacto: <a target="_blank" rel="noopener noreferrer" 
-                    href="https://wa.me/+573205119854">+56949165634</a><br/>
+                     href="mailto:jhonhtr@gmail.com">jhonhtr@gmail.com </a><br/>                
                 Derechos Reservados- Copyright 2024</p>
             </footer>            
         </div>
