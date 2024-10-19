35cc80830f695dac80542aa39bbe56c2ff432a64
jhonhtr <129766078+jhonhtr@users.noreply.github.com>
2024-10-19 12:21:43 -0500

    Update Ventanas.js

--- src/ventana/Ventanas.js
+++ src/ventana/Ventanas.js
@@ -38,11 +38,12 @@ const Ventanas = () => {
             <div className='contenedorventanas'>
                 
                 <div className='contventana1'>
+                    <div className="ref-text">ref050</div>
                     <div className='contventana2'>
                         <h4>Ventana 100% en Madera</h4>
                         <img src={ventana1} alt="ventana1" width={200} height={210}/>                        
                         <div className='contventana3'>
-                            <a className='contventana4' target="_blank" rel="noopener noreferrer" href="https://wa.me/+56949165634">Solicitar información aquí</a>
+                        <a className='contd4' target="_blank" rel="noopener noreferrer" href={`https://wa.me/56949165634?text=Solicito%20información%20sobre%20el%20producto:%20Ventana%20100%25%20en%20Madera.%20con%20ref050`}>Solicitar información aquí</a>
                         </div> 
                     </div>                                       
                 </div>            
