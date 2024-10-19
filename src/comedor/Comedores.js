f06753098299d7fe722453130e157e2ed68e58f9
jhonhtr <129766078+jhonhtr@users.noreply.github.com>
2024-10-19 12:15:53 -0500

    Update Comedores.js

--- src/comedor/Comedores.js
+++ src/comedor/Comedores.js
@@ -43,41 +43,45 @@ const Comedores = () => {
             <div className='contenedcomedor'>
                 
                 <div className='contcom1'>
+                    <div className="ref-text">ref004</div>
                     <div className='contcom2'>
                         <h4>Comedor 100% en Madera</h4>
                         <img src={come1} alt="come1" width={200} height={210}/>                        
                         <div className='contcom3'>
-                            <a className='contcom4' target="_blank" rel="noopener noreferrer" href="https://wa.me/+56949165634">Solicitar información aquí</a>
+                            <a className='contd4' target="_blank" rel="noopener noreferrer" href={`https://wa.me/56949165634?text=Solicito%20información%20sobre%20el%20producto:%20Comedor%20100%25%20en%20Madera.%20con%20ref004`}>Solicitar información aquí</a>
                         </div> 
                     </div>                                       
                 </div>
                 
                 <div className='contcom5'>
+                    <div className="ref-text">ref005</div>
                     <div className='contcom6'>
                         <h4>Comedor 100% en Madera</h4>
                         <img src={come2} alt="come2" width={200} height={210}/>
                         <div className='contcom3'>
-                            <a className='contcom4' target="_blank" rel="noopener noreferrer" href="https://wa.me/+56949165634">Solicitar información aquí</a>
+                            <a className='contd4' target="_blank" rel="noopener noreferrer" href={`https://wa.me/56949165634?text=Solicito%20información%20sobre%20el%20producto:%20Comedor%20100%25%20en%20Madera.%20con%20ref005`}>Solicitar información aquí</a>
                         </div>
                     </div>                    
                 </div>
 
                 <div className='contcom7'>
+                    <div className="ref-text">ref006</div>
                     <div className='contcom8'>
                         <h4>Comedor 100% en Madera</h4>
                         <img src={come3} alt="come3" width={200} height={210}/>
                         <div className='contcom3'>
-                            <a className='contcom4' target="_blank" rel="noopener noreferrer" href="https://wa.me/+56949165634">Solicitar información aquí</a>
+                            <a className='contd4' target="_blank" rel="noopener noreferrer" href={`https://wa.me/56949165634?text=Solicito%20información%20sobre%20el%20producto:%20Comedor%20100%25%20en%20Madera.%20con%20ref006`}>Solicitar información aquí</a>
                         </div>
                     </div>                    
                 </div>
 
                 <div className='contcom9'>
+                    <div className="ref-text">ref007</div>
                     <div className='contcom10'>
                         <h4>Comedor 100% en Madera</h4>
                         <img src={come4} alt="come4" width={200} height={210}/>
                         <div className='contcom3'>
-                            <a className='contcom4' target="_blank" rel="noopener noreferrer" href="https://wa.me/+56949165634">Solicitar información aquí</a>
+                            <a className='contd4' target="_blank" rel="noopener noreferrer" href={`https://wa.me/56949165634?text=Solicito%20información%20sobre%20el%20producto:%20Comedor%20100%25%20en%20Madera.%20con%20ref007`}>Solicitar información aquí</a>
                         </div>
                     </div>                    
                 </div>
@@ -86,32 +90,35 @@ const Comedores = () => {
 
             <div className='contenedcomedor1'>
 
-            <div className='contcom11'>
+                <div className='contcom11'>
+                    <div className="ref-text">ref008</div>
                     <div className='contcom12'>
                         <h4>Comedor 100% en Madera</h4>
                         <img src={come5} alt="come5" width={200} height={210}/>
                         <div className='contcom3'>
-                            <a className='contcom4' target="_blank" rel="noopener noreferrer" href="https://wa.me/+56949165634">Solicitar información aquí</a>
+                            <a className='contd4' target="_blank" rel="noopener noreferrer" href={`https://wa.me/56949165634?text=Solicito%20información%20sobre%20el%20producto:%20Comedor%20100%25%20en%20Madera.%20con%20ref008`}>Solicitar información aquí</a>
                         </div>
                     </div>                    
                 </div>
 
                 <div className='contcom13'>
+                    <div className="ref-text">ref009</div>
                     <div className='contcom14'>
                         <h4>Comedor 100% en Madera</h4>
                         <img src={come6} alt="come6" width={200} height={210}/>
                         <div className='contcom3'>
-                            <a className='contcom4' target="_blank" rel="noopener noreferrer" href="https://wa.me/+56949165634">Solicitar información aquí</a>
+                            <a className='contd4' target="_blank" rel="noopener noreferrer" href={`https://wa.me/56949165634?text=Solicito%20información%20sobre%20el%20producto:%20Comedor%20100%25%20en%20Madera.%20con%20ref009`}>Solicitar información aquí</a>
                         </div>
                     </div>                    
                 </div>
 
                 <div className='contcom15'>
+                    <div className="ref-text">ref010</div>
                     <div className='contcom16'>
                         <h4>Comedor 100% en Madera</h4>
                         <img src={come7} alt="come7" width={200} height={210}/>
                         <div className='contcom3'>
-                            <a className='contcom4' target="_blank" rel="noopener noreferrer" href="https://wa.me/+56949165634">Solicitar información aquí</a>
+                            <a className='contd4' target="_blank" rel="noopener noreferrer" href={`https://wa.me/56949165634?text=Solicito%20información%20sobre%20el%20producto:%20Comedor%20100%25%20en%20Madera.%20con%20ref010`}>Solicitar información aquí</a>
                         </div>
                     </div>                    
                 </div>
